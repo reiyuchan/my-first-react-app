781f39e4ca39ec22bb35da59d7cf9d80aaf1d024
reiyuchan <85351652+reiyuchan@users.noreply.github.com>
2022-01-30 08:20:04 +0200

    fixed css

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ export default function Main(props) {
         <div className="main-cont">
             <img src={props.items.imgUrl} alt="main-icon" />
             <div className='main-sub-cont'>
-            <h2><img src={locaIcon} alt="location icon" />{props.items.location}</h2>
+            <div className='location-cont'><img src={locaIcon} alt="location icon" />    
+            <h2>{props.items.location}</h2>
+            </div>
             <a href="https://maps.google.com/maps/ms?ie=UTF8&hl=en&msa=0&msid=203233677966749432467.00049d778584ad338f487&ll=35.365256,138.737411&spn=0.09799,0.145912&z=12&output=embed">View on Google Maps</a>
             <h1>{props.items.title}</h1>
             <h3>{props.items.startDate} - {props.items.endDate}</h3>
